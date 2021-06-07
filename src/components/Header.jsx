46b1a0c5e3a8330bf6901112c1e9c499255ef207
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-06-07 13:56:48 -0400

    added feature that grabs data from a random user API and displays their gender

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Header = ({ title }) => {
+  const [employees, setEmployees] = useState([])
+
+  useEffect(() => {
+    getEmployees();
+  }, [])
+
+  const getEmployees = async () => {
+    const { data: { results } } = await axios.get("https://randomuser.me/api/?results=25");
+
+    setEmployees(results)
+  }
+
+  console.log(employees);
+
     return (
         <header>
           <h1>{title}</h1>
+          {employees ? employees.map( ({ gender }, index) => <h2 key={index}>{gender}</h2>) 
+          : 
+          <h1>No Results To Display</h1>}
         </header>
     )
 }
